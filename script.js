29e5ef716ece5c7b05bb225e29f551a5c1b3ca47
rnutall <rnutall1@gmail.com>
2023-11-09 08:36:07 -0600

    refactored to function

--- script.js
+++ script.js
@@ -1,42 +1,68 @@
-// prompt user fro password
-var length = 12;
-console.log(securityPassword(length));
-
-// password length and character set
-var password = (min - 8, max -122); 
-var chars = ("");
-    var charsUppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
-    var charsLowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "", "y", "z"];
-    var charsNums = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    var charsSpecial = ["!", "@", '#', "$", "%", "&", "*", "[", "]"];
-   
-
-
-   for (chars = i = 1; i <= 122; i++);
-   var chars = Math.floor(Math.random)(0 - 122);
-   
-   return(password)
-
 // Assignment Code
-// generate password i think
+// Assignment Code
+// password criteria include [upperChars, lowerChars, numbers, specialChars];
+// variables assigned to #generate and #password
+var generateBtn = document.querySelector("#generate");
+var lowerChars = 'abcdefghijklmnopqrstuvwxyz';
+  var upperChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var numbers = '12344567890';
+  var specialChars = '!@#%^&*()_+{}';
+  var chosenOptions = "";
 
-function  generatePassword() {
-    var chars= ("");
-    for (var i = 0; i < chars.length; i++) 
-    console.log("you clicked the button");
+  // Write password to the #password input
+function writePassword() {
+  chosenOptions = "";
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+ passwordText.value = password;
+}
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
+// generate password function, initiate 'chosenOptions' variable within function,
+function generatePassword() {
+  var chosenOptions = "";
+  var result = "";
+  var length = prompt("How many characters would you like in your random password? (8 - 128)");
+  if (isNaN(length) || length < 8 || length > 128) {
+    alert("Please choose characters between 8 - 128!");
+    return generatePassword();
+  }
 
-        // Write password to the #password input
-        function writePassword() {
-            var password = generatePassword();
-            var passwordText = document.querySelector("#password");
+//confirm user for character length of 8 chars no more than 128
+var includeUpperChars = confirm("Include upper case letters?", 'yes or no?');
+var includeLowerChars = confirm("Include lower case letters?");
+var includeNumbers = confirm("Include numbers?");
+var includeSpecialChars = confirm("Include special characters?");
 
+//validate password length
+if (!includeLowerChars && !includeUpperChars && !includeNumbers && !includeSpecialChars) {
+  alert("Please choose at least 1 character type! ");
+  return generatePassword();
+}
 
-            passwordText.value = password;
+//if statement confirm characters input
+  if (includeLowerChars) {
+    chosenOptions += lowerChars;
+} 
+if (includeUpperChars) {
+  chosenOptions += upperChars;
+}
+if  (includeNumbers) {
+  chosenOptions += numbers
+} 
+if (includeSpecialChars) {
+    chosenOptions += specialChars;
+  }
 
-        }
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * chosenOptions.length);
+    result += chosenOptions.charAt(randomIndex);
+   }
 
-        // Add event listener to generate button
-        generateBtn.addEventListener("click", password);
+   return result;
+
+}
+
 
